f345173484e40e5d011b58c94a0be52bb8d654dc
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-09-22 23:45:33 +0200

    SCSS Config & Header Styling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <>
-      <p>A <span>Resume</span> that stands out!</p> 
-      <p>Make your own resume. <span>It's free !</span></p> 
-    </>
+    <div className="container">
+      <p className="heading">
+         A 
+         <span> Resume </span> that stands out!
+      </p> 
+      <p className="heading">
+         Make your own resume. 
+         <span> It's free ! </span>
+      </p> 
+    </div>
   )  
 }
 
