6330fd9debd05a036fcc14c022a515836f0a871e
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-09-23 11:33:00 +0200

    Editor

--- client/src/pages/Routes.jsx
+++ client/src/pages/Routes.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Body from '../components/Body'
 import Header from '../components/Header'
-import Editor from '../components/Editor'
 
 const Routes = () => {
   return (
