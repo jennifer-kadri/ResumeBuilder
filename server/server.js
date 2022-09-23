cbb2d63dcf22aa42c786b277121e1c63c0b2e6ec
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-09-23 11:50:23 +0200

    Server Config

--- server/server.js
+++ server/server.js
@@ -1,1 +1,36 @@
 const express = require('express');
+const app = express();
+const cors = require('cors');
+const fileUpload = require("express-fileupload");
+const server = require("http").createServer(app);
+const dotenv = require("dotenv").config();
+
+var parth = require("path");
+var bcrypt = require("bcryptjs");
+
+app.use(cors({
+   origin: "http://localhost:3000",
+   credentials: true
+}));
+
+app.use(fileUpload({
+   createParentPath: true
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+/** CONNEXION TO LOCALHOST */
+const colors = require("./assets/colors");
+const join = require("path");
+const port = process.env.PORT;
+const hostname = "localhost";
+
+server.listen(port, () => console.log(
+   `\u2794 Server up and running on port ${port}`.custom,
+   `\nat: http://${hostname}:${port}/`.brightMagenta
+));
