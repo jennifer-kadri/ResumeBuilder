0215c34de8b64dd44231fc778197f02897935163
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-09-23 20:29:02 +0200

    API Routes in React

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,3 +1,3 @@
 export const host = "http://localhost:3000";
-export const registerRoute = `${host}/auth/register`;
-export const loginRoute = `${host}/auth/login`;
+export const registerRoute = `${host}/api/auth/register`;
+export const loginRoute = `${host}/api/auth/login`;
