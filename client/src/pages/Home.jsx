23b96541800f7533c0c957990df72ae9266198aa
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-09-23 20:16:15 +0200

    Pages Setup

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/Header'
+import Body from '../components/Body'
+import Editor from '../components/Editor'
+
+const Home = () => {
+   return (
+      <>
+         <Header />
+         <Body />
+         <Editor />
+      </> 
+   )  
+ }
+ 
+ export default Home
