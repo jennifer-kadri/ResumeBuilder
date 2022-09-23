14419ba4dd404bfe2fcb49b4d9e176ae61b3355a
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-09-23 15:43:03 +0200

    Database Config

--- server/app/config/database.js
+++ server/app/config/database.js
@@ -2,9 +2,10 @@ const mysql = require("mysql2");
 
 /** CONNEXION TO DATABASE */
 const mysqlConnection = mysql.createConnection({
-  host: 'localhost',
-  user: 'miyuna',
-  password: '',
+  host: "localhost",
+  user: "miyuna",
+  password: "",
+  database: "resume_builder",
 });
 
 mysqlConnection.connect((err) => {
@@ -12,9 +13,20 @@ mysqlConnection.connect((err) => {
      return console.error("Error:" + err.message);
   }
   console.log("Database".pink,"Connection Successfull");
-  mysqlConnection.query("CREATE DATABASE resume_builder", (err, result) => {
-    console.log("Database created successfully");
-  })
+  
+  const db = "CREATE DATABASE resume_builder";
+
+  const table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTO_INCREMENT, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL)";
+
+  mysqlConnection.query(table, (err, results, fields) => {
+    console.log("Users table created successfully");
+  });
+
+  mysqlConnection.end((err) => {
+    if (err) {
+      return console.log(err.message);
+    };
+  });
 });
 
 module.exports = mysqlConnection;
