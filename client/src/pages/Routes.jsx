1c7e938968ac8b578553f29c7d4be8bc4ecd1ea1
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-09-24 13:52:50 +0200

    Base SCSS

--- client/src/pages/Routes.jsx
+++ client/src/pages/Routes.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
-import Register from '../pages/Register'
-import Login from '../pages/Login'
 
 const Pages = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />}/>
-      <Route path="/register" element={<Register />}/>
-      <Route path="/login" element={<Login />}/>
     </Routes>
   )  
 }
