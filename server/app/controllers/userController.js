05ca5b102718582845fc1259176f2b2c6aeafc88
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-09-24 18:14:04 +0200

    User Controller with JWT Token

--- server/app/controllers/userController.js
+++ server/app/controllers/userController.js
@@ -1,37 +1,141 @@
-const User = require('../models/userModel');
-const bcrypt = require('bcryptjs');
+const config = require("../../config/auth.config.js");
+const db = require("../models");
+const Op = db.Sequelize.Op;
+const User = db.user;
+const refreshToken = db.RefreshToken;
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { TokenExpiredError } = jwt;
 
+/**
+* @description Test user private content with JWT auth
+* @param req
+* @param res
+*/
+exports.userBoard = (req, res) => {
+   res.status(200).send("User Content.");
+};
 
+/** REGISTER **/
 module.exports.register = async (req, res, next) => {
-   try {
-      const { email, password } = req.body;
-      const emailCheck = await User.findOne(email);
-      if ( emailCheck )
-         return res.json({ msg: "Email already in use", status: false });
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const user = await User.create({
-         email, 
-         password: hashedPassword,
+   User.create({
+      email: req.body.email,
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      civility: req.body.civility,
+      username: req.body.username,
+      password: bcrypt.hashSync(req.body.password, 8)
+  })
+   .catch(err => {
+      res.status(500).send({ message: err.message });
+   });
+};
+
+/** LOGIN **/
+module.exports.login = async (req, res, next) => {
+   User.findOne({
+      where: {
+          email: req.body.email
+      }
+  })
+   .then(async (user) => {
+      if (!user) {
+           return res.status(404).send({ message: "We didn't find you, sorry." });
+      }
+      const passwordIsValid = bcrypt.compareSync(
+         req.body.password,
+         user.password
+      );
+      if (!passwordIsValid) {
+         return res.status(401).send({
+            accessToken: null,
+            message: "Invalid password, retry."
+         });
+      }
+      const token = jwt.sign({ id: user.id }, config.secret, {
+         expiresIn: config.jwtExpiration
       });
-      delete user.password;
-      return res.json({ status: true, user });
-   } catch (ex) {
-      next(ex);
+      let refreshToken = await RefreshToken.createToken(user);
+         res.status(200).send({
+            id: user.id,
+            email: user.email,
+            accessToken: token,
+            refreshToken: refreshToken,
+            message: 'Successfully logged in !'
+         });
+      })
+      .catch(err => {
+         res.status(500).send({ message: err.message });
+      });
+};
+
+/** LOGOUT **/
+module.exports.logout = (req, res) => {
+   try {
+       RefreshToken.destroy({ where: { userId: req.userId } });
+
+       res.status(200).json({
+           message: "Successfully logged out !",
+       });
+       return;
+   } catch (err) {
+       return res.status(500).send({ message: err });
    }
 };
 
-module.exports.login = async (req, res, next) => {
+/** TOKEN REFRESH **/
+exports.refreshToken = async (req, res) => {
+   const { refreshToken: requestToken } = req.body;
+   if (requestToken == null) {
+       return res.status(403).json({ message: "Refresh Token is required!" });
+   }
    try {
-      const { email, password } = req.body;
-      const user = await User.findOne(email);
-      if ( !user ) 
-         return res.json({ msg: "Incorrect email or password, please retry.", status: false });
-      const isPasswordValid = await bcrypt.compare(password, user.password);
-      if ( !isPasswordValid ) 
-         return res.json({ msg: "Incorrect email or password, please retry.", status: false });
-      delete user.password;
-      return res.json({ status: true, user });
-   } catch (ex) {
-      next(ex);
+       let refreshToken = await RefreshToken.findOne({ where: { token: requestToken } });
+       console.log(refreshToken)
+       if (!refreshToken) {
+           res.status(403).json({ message: "Refresh token is not in database!" });
+           return;
+       }
+       if (RefreshToken.verifyExpiration(refreshToken)) {
+           RefreshToken.destroy({ where: { id: refreshToken.id } });
+
+           res.status(403).json({
+               message: "Refresh token was expired. Please make a new signin request",
+           });
+           return;
+       }
+       const user = await refreshToken.getUser();
+       let newAccessToken = jwt.sign({ id: user.id }, config.secret, {
+           expiresIn: 86400, // 24 hours
+       });
+       return res.status(200).json({
+           accessToken: newAccessToken,
+           refreshToken: refreshToken.token,
+       });
+   } catch (err) {
+       return res.status(500).send({ message: err });
    }
 };
+
+/** GET INFOS **/
+module.exports.getUserInfos = (req, res) => {
+   let token = req.headers["x-access-token"];
+   var userId;
+   if (!token) {
+       return res.status(403).send({
+           message: "No token provided!"
+       });
+   }
+   jwt.verify(token, config.secret, (err, decoded) => {
+       if (err) {
+           return catchError(err, res);
+       }
+       req.userId = decoded.id;
+       userId = decoded.id;
+   });
+   User.findOne({
+      where: {
+         id: userId
+      },
+   })
+}
