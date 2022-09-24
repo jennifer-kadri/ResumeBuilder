9fe30edd093410180a7fd7b39d0822ef7d811158
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-09-24 22:22:43 +0200

    Routes Changes

--- client/src/pages/Routes.jsx
+++ client/src/pages/Routes.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import NavBar from '../components/Navbar'
 import Home from '../pages/Home'
 import Login from './Login'
 import Register from './Register'
 
 const Pages = () => {
   return (
+    <>
+    <NavBar />
     <Routes>
       <Route path="/" element={<Home />}/>
       <Route path="/signup" element={<Register />}/>
       <Route path="/login" element={<Login />}/>
     </Routes>
+    </>
   )  
 }
 
