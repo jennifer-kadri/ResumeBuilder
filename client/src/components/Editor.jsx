558767a2cffc0fbf084c1d610eb31964f1370450
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-09-24 17:41:14 +0200

    Server Changes

--- client/src/components/Editor.jsx
+++ client/src/components/Editor.jsx
@@ -9,6 +9,7 @@ const Editor = (props) => {
    const [activeSection, setActiveSection] = useState(Object.keys(sections)[0]
    );
 
+   /** WORK DATA **/
    const workExpBody = (
       <div className="detail">
          <div className="row">
@@ -58,6 +59,7 @@ const Editor = (props) => {
       </div>
    )
 
+   /** PROJECT DATA **/
    const projectBody = (
       <div className="detail">
          <div className="row">
@@ -87,6 +89,91 @@ const Editor = (props) => {
 
          <div className="column">
             <label>Enter project description</label>
+            <InputControl placeholder="Line 1" />
+            <InputControl placeholder="Line 2" />
+            <InputControl placeholder="Line 3" />
+         </div>
+      </div>
+   )
+
+   /** EDUCATION DATA **/
+   const eduBody = (
+      <div className="detail">
+         <div className="row">
+            <InputControl 
+               label="Title"
+               placeholder="Enter title eg. Education"
+            />
+         </div>
+            <InputControl 
+               label="College/School Name"
+               placeholder="Enter name of your college/school"
+            />
+         <div className="row">
+            <InputControl 
+               label="Start Date"
+               placeholder="Enter start date this education"
+            />
+         </div>
+         <div className="row">
+            <InputControl 
+               label="Location"
+               placeholder="Enter end date of this education"
+            />
+         </div>
+      </div>
+   )
+
+   /** PROJECT DATA **/
+   const infoBody = (
+      <div className="detail">
+         <div className="row">
+            <InputControl 
+               label="Firstname"
+               placeholder="Enter your firstname"
+            />
+            <InputControl 
+               label="Lastname"
+               placeholder="Enter your lastname"
+            />
+            <InputControl 
+               label="Title"
+               placeholder="Enter your title eg. Frontend Developer"
+            />
+         </div>
+         <div className="row">
+            <InputControl 
+               label="Email Address"
+               placeholder="Enter your email address eg. miyudev@mail.com"
+            />
+         </div>
+         <div className="row">
+            <InputControl 
+               label="Mobile Number"
+               placeholder="Enter your phone number"
+            />
+         </div>
+         <div className="row">
+            <InputControl 
+               label="Linkedin Link"
+               placeholder="Enter your linkedin profile link"
+            />
+         </div>
+         <div className="row">
+            <InputControl 
+               label="Github Link"
+               placeholder="Enter your github profile link"
+            />
+         </div>
+         <div className="row">
+            <InputControl 
+               label="Location"
+               placeholder="Enter Github link of project"
+            />
+         </div>
+
+         <div className="column">
+            <label>Enter your bio description</label>
             <InputControl placeholder="Line 1" />
             <InputControl placeholder="Line 2" />
             <InputControl placeholder="Line 3" />
