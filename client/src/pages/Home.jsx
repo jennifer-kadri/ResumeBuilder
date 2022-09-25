41318f545107af56f18ef6386b14e08db373d891
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-09-25 15:24:32 +0200

    Home Page Changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-import NavBar from "../components/Navbar";
 import Header from "../components/Header";
-import Body from "../components/Body";
 import styled from "styled-components";
 import { Link, useLocation } from "react-router-dom"
-import Editor from "../components/Editor";
 
 const Home = () => {
    return (
