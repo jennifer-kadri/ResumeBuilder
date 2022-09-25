ac5612da86b05921982532c46c3869805ed4aa3b
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-09-25 15:07:48 +0200

    MenuItems component

--- client/src/components/Dropdown/MenuItems.jsx
+++ client/src/components/Dropdown/MenuItems.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from "react";
-import { User } from "react-feather";
 import { Link } from "react-router-dom";
 import Dropdown from "./Dropdown";
 import styled from "styled-components";
