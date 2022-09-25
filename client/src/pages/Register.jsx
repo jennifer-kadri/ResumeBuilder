c90169a43c9e0d546bbdfce8f3dab67141e939e1
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-09-25 22:47:16 +0200

    Register Page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,181 +1,190 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
+import { Link, useNavigate } from "react-router-dom";
+// import styled from "styled-components";
 import AuthService from "../components/Auth/auth.service";
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
+import { isEmail } from "validator";
 
-const Register = ({ required, validEmail, validPassword, validUsername }) => {
+const required = (value) => {
+   if (!value) {
+      return (
+         <div className="error">
+            This field is required.
+         </div>
+      );
+   };
+};
+
+const validEmail = (value) => {
+   if (!isEmail(value)) {
+      return (
+         <div className="error">
+            This email address is not valid.
+         </div>
+      );
+   };
+};
+
+const validPassword = (value) => {
+   if (value.length < 6 || value.length > 40) {
+      return (
+         <div className="error">
+            The password must be between 6 and 40 characters long.
+         </div>
+      );
+   };
+};
+
+const validUsername = (value) => {
+   console.log(value);
+   if (value === null | value === "") {
+      return (
+         <div>
+            The username is required.
+         </div>
+      );
+   };
+};
+
+const Register = (props) => {
+   const navigate = useNavigate();
+   const form = useRef();
+   const checkBtn = useRef();
+
    const [firstname, setFirstname] = useState("");
    const [lastname, setLastname] = useState("");
    const [username, setUsername] = useState("");
    const [email, setEmail] = useState("");
    const [password, setPassword] = useState("");
    const [confirmPassword, setConfirmPassword] = useState("");
-   
    const [message, setMessage] = useState("");
-   const [submitted, setSubmitted] = useState(false);
-   const [error, setError] = useState(false);
+   const [successful, setSuccessful] = useState(false);
+   const [errorMessage, setErrorMessage] = useState(false);
+   const [isDirty, setIsDirty] = useState(false);
 
-   useEffect(() => {
-      if (confirmPassword !== password) {
-         if (password === confirmPassword) setError(false);
-            else setError(true);
+   useEffect((password, isDirty) => {
+      if (isDirty) {
+         if (password === confirmPassword) {
+            setErrorMessage(false);
+         } else {
+            setErrorMessage(true);
+         }
       }
    }, [confirmPassword]);
 
    const handleFirstname = (event) => {
       setFirstname(event.target.value);
-      setSubmitted(false);
    };
 
    const handleLastname = (event) => {
       setLastname(event.target.value)
-      setSubmitted(false);
    };
 
    const handleUsername = (event) => {
       setUsername(event.target.value)
-      setSubmitted(false);
    };
 
    const handleEmail = (event) => {
       setEmail(event.target.value);
-      setSubmitted(false);
    };
 
    const handlePassword = (event) => {
       setPassword(event.target.value);
-      setSubmitted(false);
    };
    
    const handleConfirmPassword = (event) => {
       setConfirmPassword(event.target.value);
-      setSubmitted(false);
+      setIsDirty(true);
    };
 
    const handleSubmit = (event) => {
       event.preventDefault();
 
       setMessage("");
-      setSubmitted(false);
+      setSuccessful(false);
 
-      if (!error) {
+      form.current.validateAll();
+
+      if (!errorMessage) {
          AuthService.register(firstname, lastname, username, email, password, confirmPassword).then(
             (response) => {
                console.log(response.data.message);
-               setSubmitted(true);
+               setMessage(response.data.message);
+               setSuccessful(true);
+               setTimeout(() => {
+                  navigate("/")
+               }, 2000)
             },
             (error) => {
-               const Msg = (
+               const resMessage = (
                   error.response && error.response.data && error.response.data.message
                ) || error.message || error.toString();
 
-               setMessage(Msg);
-               setSubmitted(false);
+               setMessage(resMessage);
+               setSuccessful(false);
             }
-         )
+         );
       }
-   };
-
-   const successMessage = () => {
-      return (
-         <div 
-            className="success"
-            style={{ display: submitted ? '' : 'none', }}
-         >
-            <h1>User {username} successfully registered !</h1>
-         </div>
-      );
-   };
-
-   const errorMessage = () => {
-      return (
-         <div 
-            className="errror"
-            style={{ display: error ? '' : 'none', }}
-         >
-            <h1>Please enter all the fields.</h1>
-         </div>
-      );
    };
 
    return (
       <>
          <div id="register">
-            <div className="div"><h1>Sign Up</h1></div>
+            <div className="container">
+               <div className="div"><h1>Create your account</h1></div>
+               <Form className="form" onSubmit={handleSubmit} ref={form}>
+                  {!successful && (
+                     <>
+                        <div className="form-group">
+                           <Input type="text" onChange={handleFirstname} className="form-input" value={firstname} validations={[required]}/>
+                        </div>
+                     
+                        <div className="form-group">
+                           <Input type="text" onChange={handleLastname} className="form-input" value={lastname} validations={[required]}/>
+                        </div>
 
-            <div className="messages">
-               {errorMessage()}
-               {successMessage()}
-            </div>
+                        <div className="form-group">
+                           <Input type="text" onChange={handleUsername} className="form-input" value={username} validations={[required, validUsername]}/>
+                        </div>
 
-            <form className="form">
-               <input 
-                  type="text" 
-                  className="form-input" 
-                  name="firstname"
-                  placeholder="Firstname"
-                  value={firstname} 
-                  onChange={handleFirstname} 
-                  validations={[required]}
-               />
-            
-               <input 
-                  type="text" 
-                  className="form-input" 
-                  name="lastname" 
-                  placeholder="Lastname"
-                  value={lastname} 
-                  onChange={handleLastname} 
-                  validations={[required]}
-               />
+                        <div className="form-group">
+                           <Input type="text" onChange={handleEmail} className="form-input" value={email} validations={[required, validEmail]}/>
+                        </div>
 
-               <input 
-                  type="text" 
-                  className="form-input" 
-                  name="username" 
-                  placeholder="Username"
-                  value={username} 
-                  onChange={handleUsername} 
-                  validations={[required]}
-               />
+                        <div className="form-group">
+                           <Input type="text" onChange={handlePassword} className="form-input" value={password} validations={[required, validPassword]}/>
+                        </div>
 
-               <input 
-                  type="text" 
-                  className="form-input" 
-                  name="email" 
-                  placeholder="Email"
-                  value={email} 
-                  onChange={handleEmail} 
-                  validations={[required, validEmail]}
-               />
+                        <div className="form-group">
+                           <Input type="text" onChange={handleConfirmPassword} className="form-input" value={confirmPassword} validations={[required]}/>
+                           {errorMessage && isDirty ? 
+                              <div className="invalid">
+                                 Password did not match
+                              </div> : ""
+                           }
+                        </div>
+                     
+                        <button type="submit" className="btn btn-submit">Register</button>
+                     </>
+                  )}
+               </Form>
 
-               <input 
-                  type="text" 
-                  className="form-input" 
-                  name="password" 
-                  placeholder="Password"
-                  value={password} 
-                  onChange={handlePassword} 
-                  validations={[required, validPassword]}
-               />
-
-               <input 
-                  type="text" 
-                  className="form-input" 
-                  name="confirm-password" 
-                  placeholder="Confirm password"
-                  value={confirmPassword} 
-                  onChange={handleConfirmPassword}
-                  validations={[required]}
-               />
-            
-               <button type="submit" onClick={handleSubmit} className="btn btn-submit">Register</button>
+               {message && (
+                  <div className="form-group">
+                      <div className={successful ? "alert alert-success" : "alert alert-danger"} role="alert" >
+                          {message}
+                     </div>
+                  </div>
+               )}
+                    
 
                <div className="account">
                   <span>Already have an account?</span>
                   <Link to="/login">Log in</Link>
                </div>
-            </form>
+            </div>
          </div>
       </> 
    )  
