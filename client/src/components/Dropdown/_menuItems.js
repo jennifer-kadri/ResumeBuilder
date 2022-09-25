46204fc5fc2b5ce2a64e346630e7c9e1a7448df0
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-09-25 14:53:38 +0200

    Navbar Component with Dropdown Menu

--- client/src/components/Dropdown/_menuItems.js
+++ client/src/components/Dropdown/_menuItems.js
@@ -4,15 +4,18 @@ export const menuItems = [
    {
      title: <User />,
      url: '/profile',
+     cname: 'icon',
      submenu: [
        {
          title: 'Login',
          url: 'login',
+         cname: 'icon',
        },
        {
         title: 'Signup',
         url: 'signup',
-      },
+        cname: 'icon',
+    },
      ],
    },
  ];
