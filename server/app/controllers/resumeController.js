7868c1e3e85d9a0a145663391b900d478e50d1fd
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-09-26 22:15:21 +0200

    Resume Controller

--- server/app/controllers/resumeController.js
+++ server/app/controllers/resumeController.js
@@ -61,3 +61,107 @@ module.exports.findAll = async (req, res) => {
       res.status(500).send({ message: err.message || "Some error occurred while retrieving the resumes." });
    });
 }
+
+/**
+ * @description Find a resume with id in request
+ * @param req 
+ * @param res 
+ */
+module.exports.findOne = async (req, res) => {
+   const id = req.params.id;
+   await Resume.findByPk(id, {})
+      .then(data => {
+         if (data) {
+            res.send(data);
+         } else {
+            res.status(404).send({
+               message: `Couldn't find the resume with id=${id}.`
+            });
+         }
+      })
+      .catch(err => {
+         res.status(500).send({
+            message: "There has been an error while retrieving the resume with id=" + id
+         });
+      });
+};
+
+
+/**
+ * @description Update a resume by the id in request
+ * @param req 
+ * @param res 
+ */
+module.exports.update = (req, res) => {
+   const id = req.params.id;
+   Resume.update(req.body, {
+      where: { id: id }
+   })
+   .then(nb => {
+      if (nb == 1) {
+         res.send({
+            message: "Resume was updated successfully."
+         });
+      } else {
+         res.send({
+            message: `Update of resume with id=${id} failed.`
+         });
+      }
+   })
+   .catch(err => {
+      res.status(500).send({
+         message: "There has been an error while updating the resume with id=" +id
+      });
+   });
+};
+
+
+/**
+ * @description Delete a resume by the id in request
+ * @param req 
+ * @param res 
+ */
+module.exports.delete = (req, res) => {
+   const id = req.params.id;
+   Resume.destroy({
+      where: { id: id }
+   })
+   .then(nb => {
+      if (nb == 1) {
+         res.send({
+            message: "Resume was deleted successfully."
+         });
+      } else {
+         res.send({
+            message: `Suppression of resume with id=${id} failed.`
+         });
+      }
+   })
+   .catch(err => {
+      res.status(500).send({
+         message: "There has been an error while removing the resume with id=" +id
+      });
+   });
+};
+
+
+/**
+ * @description Delete all resumes
+ * @param req 
+ * @param res 
+ */
+module.exports.deleteAll = (req, res) => {
+   Resume.destroy({
+      where: {},
+      truncate: false
+   })
+   .then(nb => {
+      res.send({ message: `${nb} resumes were deleted successfully!` });
+  })
+  .catch(err => {
+      res.status(500).send({
+          message:
+              err.message || "Some error occurred while removing all resumes."
+      });
+  });
+};
