6ef3d6ec2f71af0f8e1b735e75af03d1ea2449e5
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-09-27 20:54:49 +0200

    Index Model Setup

--- server/app/models/index.js
+++ server/app/models/index.js
@@ -21,15 +21,27 @@ db.sequelize = sequelize;
 
 db.user = require("./user.model.js")(sequelize, Sequelize);
 db.refreshToken = require("./refreshToken.js")(sequelize, Sequelize);
-db.resume = require("./resumeModel.js")(sequelize, Sequelize);
+db.resume = require("./resume/resume.model.js")(sequelize, Sequelize);
+db.resumeInfo = require("./resume/resumeInfo.model.js")(sequelize, Sequelize);
+db.resumeWork = require("./resume/resumeWork.model.js")(sequelize, Sequelize);
+db.resumeProject = require("./resume/resumeProject.model.js")(sequelize, Sequelize);
+db.resumeEdu = require("./resume/resumeEdu.model.js")(sequelize, Sequelize);
+db.resumeExtra = require("./resume/resumeExtra.model.js")(sequelize, Sequelize);
+
+db.user.hasOne(db.refreshToken, {
+  foreignKey: "userId",
+  targetKey: "id"
+});
 
 db.refreshToken.belongsTo(db.user, {
   foreignKey: "userId",
   targetKey: "id"
 });
-db.user.hasOne(db.refreshToken, {
-  foreignKey: "userId",
-  targetKey: "id"
-});
+
+db.resumeInfo.belongsTo(db.resume, { as: "info" });
+db.resumeWork.belongsTo(db.resume, { as: "work" });
+db.resumeProject.belongsTo(db.resume, { as: "project"});
+db.resumeEdu.belongsTo(db.resume, { as: "education"});
+db.resumeExtra.belongsTo(db.resume, { as: "extra"});
 
 module.exports = db;
