de3101fc1c354556c8c222a28f52d65be46ba75f
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-09-28 18:31:44 +0200

    Resume Model

--- server/app/models/resume/resume.model.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, Sequelize) => {
-   const Resume = sequelize.define("resumes", {
-       title: {
-            type: Sequelize.STRING
-       },
-   });
-   
-   return Resume;
-};
