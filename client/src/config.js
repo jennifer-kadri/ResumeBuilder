728b62dfe3009725fe790ccf3f75db2e1dce0b16
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-09-28 20:46:36 +0200

    Last changes

--- client/src/config.js
+++ client/src/config.js
@@ -3,7 +3,7 @@ const port = "http://localhost:8000";
 const API_PUBLIC_URL = `${port}`;
 const API_BASE_URL = `${port}/api`;
 const API_AUTH_URL = `${port}/api/auth/`;
-const API_CREATE_URL = `${port}/resume/`;
+const API_CREATE_URL = `${port}/api/resume/`;
 
 export {
     API_PUBLIC_URL,
